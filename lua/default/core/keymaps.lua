848813a007e3773e864e1243c771ac53aa542358
Ashitom <sasukeuchia32828@gmail.com>
2023-11-08 00:12:34 +0900

    Add Config

---
+++ lua/default/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general keymaps
+
+keymap.set("i", "jk", "<ESC>")
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- Telescope
+
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+keymap.set("n", "<leader>rf", "<cmd>NvimTreeRefresh<cr>")
